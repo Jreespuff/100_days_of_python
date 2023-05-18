6e854e84e28cf6a4647354dfe57f6d632e65714f
Jrees <jacobrees15@gmail.com>
2023-05-18 10:29:14 -0600

    day one code

---
+++ Day_1.py
@@ -0,0 +1,6 @@
+#Program to generate a band name based on city and first pets name
+
+print('Welcome to Band Name Generator')
+city = input("What's the city you grew up in? ")
+pet = input("What was your first pet's name? ")
+print("A band name you could use is " + city , pet + ".")
